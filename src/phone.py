89dc0d78d6d2461807b103721b66543daf86d35a
semyon <odintsovvans@gmail.com>
2023-07-10 18:30:10 +0600

    added ValueError

--- src/phone.py
+++ src/phone.py
@@ -6,11 +6,22 @@ class Phone(Item):
 
     def __init__(self, name, price, quantity, number_of_sim):
         super().__init__(name, price, quantity)
-        self.number_of_sim = number_of_sim
+        self.__number_of_sim = number_of_sim
 
 
     def __repr__(self):
         return f"Phone('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
+
+
+    @property
+    def number_of_sim(self):
+        return self.__number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, value):
+        if value <= 0 and value % 1 == 0:
+            raise ValueError("ValueError: the amount of sim-cards are must be integer and above zero.")
+        self.__number_of_sim = value
 
 
     def __add__(self, other):
